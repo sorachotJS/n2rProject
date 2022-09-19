50ccede2dfd65b9954d3b92770fcd52cd9d612c8
sc.sorachot@gmail.com <sc.sorachot@gmail.com>
2022-09-19 14:38:33 +0700

    first commit

---
+++ shared/slide-bar.php
@@ -0,0 +1,131 @@
+<header class="main-nav">
+          <div class="sidebar-user text-center"><a class="setting-primary" href="javascript:void(0)"><i data-feather="settings"></i></a><img class="img-90 rounded-circle" src="assets/images/dashboard/1.png" alt="">
+            <div class="badge-bottom"><span class="badge badge-primary">New</span></div><a href="user-profile.html">
+              <h6 class="mt-3 f-14 f-w-600">Sorachot Jaisat</h6></a>
+            <p class="mb-0 font-roboto">CEO Developer</p>
+            <ul>
+              <li><span><span class="counter">19.8</span>k</span>
+                <p>Follow</p>
+              </li>
+              <li><span>6 year</span>
+                <p>Experince</p>
+              </li>
+              <li><span><span class="counter">95.2</span>k</span>
+                <p>Follower </p>
+              </li>
+            </ul>
+          </div>
+          <nav>
+            <div class="main-navbar">
+              <div class="left-arrow" id="left-arrow"><i data-feather="arrow-left"></i></div>
+              <div id="mainnav">           
+                <ul class="nav-menu custom-scrollbar">
+                  <li class="back-btn">
+                    <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2" aria-hidden="true"></i></div>
+                  </li>
+                  <li class="sidebar-main-title">
+                    <div>
+                      <h6>User Management             </h6>
+                    </div>
+                  </li>
+                  <li class="dropdown"><a class="nav-link menu-title" href="javascript:void(0)"><i data-feather="users"></i><span>User</span></a>
+                    <ul class="nav-submenu menu-content">
+                      <li><a href="index.html">User Dashboard</a></li>
+                    </ul>
+                  </li>
+                  <!-- <li class="dropdown"><a class="nav-link menu-title" href="javascript:void(0)"><i data-feather="airplay"></i><span>Widgets</span></a>
+                    <ul class="nav-submenu menu-content">
+                      <li><a href="general-widget.html">General</a></li>
+                      <li><a href="chart-widget.html">Chart</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="nav-link menu-title" href="javascript:void(0)"><i data-feather="layout"></i><span>Page layout</span></a>
+                    <ul class="nav-submenu menu-content">
+                      <li><a href="box-layout.html">Boxed</a></li>
+                      <li><a href="layout-rtl.html">RTL</a></li>
+                      <li><a href="layout-dark.html">Dark</a></li>
+                      <li><a href="footer-light.html">Footer Light</a></li>
+                      <li><a href="footer-dark.html">Footer Dark</a></li>
+                      <li><a href="footer-fixed.html">Footer Fixed</a></li>
+                    </ul>
+                  </li> -->
+                  <li class="sidebar-main-title">
+                    <div>
+                      <h6>Blog Management             </h6>
+                    </div>
+                  </li>
+                  <li class="dropdown"><a class="nav-link menu-title" href="javascript:void(0)"><i data-feather="edit-3"></i><span>News</span></a>
+                    <ul class="nav-submenu menu-content">
+                      <li><a href="?page=news">News Dashboard</a></li>
+                      <li><a href="?page=news-create">News Create</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="nav-link menu-title" href="javascript:void(0)"><i data-feather="edit-3"></i><span>Article</span></a>
+                    <ul class="nav-submenu menu-content">
+                      <li><a href="news">Article Dashboard</a></li>
+                      <li><a href="news-create">Article Create</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="nav-link menu-title" href="javascript:void(0)"><i data-feather="edit-3"></i><span>Knowledge</span></a>
+                    <ul class="nav-submenu menu-content">
+                      <li><a href="knowledge">Knowledge Dashboard</a></li>
+                      <li><a href="knowledge-create">Knowledge Create</a></li>
+                    </ul>
+                  </li>
+
+                  <li class="sidebar-main-title">
+                    <div>
+                      <h6>Forum Management</h6>
+                    </div>
+                  </li>
+                  <li class="dropdown">          <a class="nav-link menu-title" href="javascript:void(0)"><i data-feather="sliders"></i><span>Forum              </span></a>
+                    <ul class="nav-submenu menu-content">
+                      <li><a href="forum">Forum Dashboard</a></li>
+                      <li><a href="forum-create">Forum Create</a></li>
+                    </ul>
+                  </li>
+                
+                  <li class="sidebar-main-title">
+                    <div>
+                      <h6>Room management</h6>
+                    </div>
+                  </li>
+                  <li class="dropdown">          <a class="nav-link menu-title" href="javascript:void(0)"><i data-feather="activity"></i><span>Rooms            </span></a>
+                    <ul class="nav-submenu menu-content">
+                      <li><a href="bootstrap-basic-table.html">Private Room</a></li>
+                      <li><a href="bootstrap-basic-table.html">Memory Room</a></li>
+                      <li><a href="bootstrap-basic-table.html">Learning Room</a></li>
+                    </ul>
+                  </li>
+               
+                  <li class="sidebar-main-title">
+                    <div>
+                      <h6>Other Management             </h6>
+                    </div>
+                  </li>
+                  <li class="dropdown">          <a class="nav-link menu-title" href="javascript:void(0)"><i data-feather="box"></i><span>Other                </span></a>
+                    <ul class="nav-submenu menu-content">
+                    <li><a href="datatable-basic-init.html">เกจิ อาจารย์</a></li>
+                      <li><a href="datatable-advance.html">ประวัติเกจิ อาจารย์</a></li>
+                    </ul>
+                 
+                  </li>
+                  <li class="sidebar-main-title">
+                    <div>
+                      <h6>Event Management             </h6>
+                    </div>
+                  </li>
+                  <li class="dropdown"><a class="nav-link menu-title" href="javascript:void(0)"><i data-feather="calendar"></i><span>Calendar</span></a>
+                    <ul class="nav-submenu menu-content">
+                      <li><a href="gallery.html">event calendar</a></li>
+                    </ul>
+                  </li>
+                  
+                 
+                 
+                </ul>
+              </div>
+              <div class="right-arrow" id="right-arrow"><i data-feather="arrow-right"></i></div>
+            </div>
+          </nav>
+        </header>
